0faf70a0632d09c1f787c6b9514ff9f9642d3023
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-08-30 00:04:11 -0600

    add assertions to test file

--- tests/webdriverUniversityTest.js
+++ tests/webdriverUniversityTest.js
@@ -1,20 +1,25 @@
+let assert = require('assert');
+
+
 describe("Verify whether webdriveruniversity links on homepage work correctly", function() {
 	it("check that the contact us button opens the contact us page", function(done) {
 		browser.setViewportSize({
 			width: 1200,
 			height: 800
 		})
-		browser.url('/');
-		var title = browser.getTitle();
+		browser.url('/'); //base url
+		let title = browser.getTitle();
+		assert.equal(title, 'WebDriverUniversity.com');
 		console.log('Title is: ' + title);
 		browser.click("#contact-us");
 		browser.pause(3000);
 	});
 
 	it("check that the login button opens the login portal page", function(done) {
-		browser.url('/');
+		browser.url('/'); // base url
 		browser.click('#login-portal');
-		var title = browser.getTitle();
+		let title = browser.getTitle();
+		assert.equal(title, 'WebDriverUniversity.com');
 		console.log('Title is: ' + title);
 		browser.pause(3000);
 	});
