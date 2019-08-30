db0df48bbdd9519c2395e17dd2621436dda58524
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-08-30 13:36:20 -0600

    clean up code formatting

--- tests/ajaxClickTest.js
+++ tests/ajaxClickTest.js
@@ -2,7 +2,7 @@ describe('Test that the button is clickable since the Ajax loader complets loadi
   it('Attempt to click the button asap', function(done) {
     browser.url('/Ajax-Loader/index.html');
     browser.click('#button1');
-  })
+  });
 
   it('Attempt to click the button after 7 seconds', function(done) {
     browser.url('/Ajax-Loader/index.html');
@@ -10,5 +10,5 @@ describe('Test that the button is clickable since the Ajax loader complets loadi
     browser.pause(7000);
     browser.click('#button1');
     browser.pause(7000);
-  })
-})
+  });
+});
