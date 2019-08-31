f0492d0433101131f37ea942d497a798af48b971
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-08-31 11:31:10 -0600

    add css locators to contact  us test file

--- tests/contactUsTest.js
+++ tests/contactUsTest.js
@@ -4,7 +4,11 @@ beforeEach(function() {
 
 describe('Test Contact Us form WebdriverUni', function() {
   it('Should be able to submit a successful submission via contact us form', function(done) {
-
+    [name='first_name']
+    [name='last_name']
+    [name='email']
+    [name='message']
+    [type='submit']
   });
 
   it('Should not be able to submit a successful submission via contact us form as all fields are required', function(done) {
