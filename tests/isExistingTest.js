e2a854bfe23a5b9fa2e9ac9b6bc7aa90b8fa04ed
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-09-02 16:34:04 -0600

    complete isExisting test cases for hidden elements page

--- tests/isExistingTest.js
+++ tests/isExistingTest.js
@@ -3,24 +3,33 @@ beforeEach(function() {
 })
 
 describe('Test whether hidden elements exist', function() {
-  it('Button display is set to non display but still exists in html dom therefore should return true', function(done) {
-    #not-displayed
-  
+  it('Button display is set to non display but still exists in html dom therefore should return true', function(done) {   
+    let isEnabled = browser.isExisting('#not-displayed');
+    console.log(isEnabled);
+    expect(isEnabled).to.equal(true);
   });
 
   it('Button display is set to visibility hidden but still exists in html dom therefore should return true', function(done) {
-    #visibility-hidden
+    let isEnabled = browser.isExisting('#visibility-hidden');
+    console.log(isEnabled);
+    expect(isEnabled).to.equal(true);
   });
 
   it('Button display is set to zero opacity but still exists in html dom therefore should return true', function(done) {
-    #zero-opacity
+    let isEnabled = browser.isExisting('#zero-opacity');
+    console.log(isEnabled);
+    expect(isEnabled).to.equal(true);
   });
 
-  it('Header text is visible and exists in the html dom therefore should return true', function(done) {
-    h1
+  it('Header text is visible and exists in the html dom therefore should return true', function(done) {   
+    let isEnabled = browser.isExisting('h1');
+    console.log(isEnabled);
+    expect(isEnabled).to.equal(true);
   });
   
-  it('There is no such element with the id of #no-such-element within the html dom therefore should return false', function(done) {
-    #no-such-element
+  it('There is no such element with the id of #no-such-element within the html dom therefore should return false', function(done) {   
+    let isEnabled = browser.isExisting('#no-such-element');
+    console.log(isEnabled);
+    expect(isEnabled).to.equal(false);
   });
 });
