bba90e55fe474e8d1c67941d375099e469963cb3
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-09-02 17:12:33 -0600

    define values for isEnabled test

--- tests/isEnabledTest.js
+++ tests/isEnabledTest.js
@@ -9,22 +9,22 @@ beforeEach(function() {
 
 describe('Test Enabled Dropdown Menus, Checkboxes & Radio Buttons', function() {
   it('Dropdown item orange is disabled therefore should return false', function(done) {
-
+    option[value='orange']
   });
   
   it('Dropdown item grape is enabled therefore should return true', function(done) {
-
+    option[value='grape']
   });
   
   it('Option2 is enabled therefore should return true', function(done) {
-
+    input[value='option-2']
   });
   
   it('Radio button pumpkin is enabled therefore should be true', function(done) {
-
+    input[value='pumpkin']
   });
   
   it('Radio button cabbage is disabled therefore should be false', function(done) {
-
+    input[value='cabbage']
   });
 });
