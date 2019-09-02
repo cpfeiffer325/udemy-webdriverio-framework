92f0a26999dc512dcb902018c0cd53ed96d145f5
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-09-02 17:05:00 -0600

    create base file for isEnabled test

---
+++ tests/isEnabledTest.js
@@ -0,0 +1,30 @@
+beforeEach(function() {
+  browser.url("/Dropdown-Checkboxes-RadioButtons/index.html");
+  browser.setViewportSize({
+		width: 1200,
+		height: 800
+	})
+	browser.pause(2000);
+})
+
+describe('Test Enabled Dropdown Menus, Checkboxes & Radio Buttons', function() {
+  it('Dropdown item orange is disabled therefore should return false', function(done) {
+
+  });
+  
+  it('Dropdown item grape is enabled therefore should return true', function(done) {
+
+  });
+  
+  it('Option2 is enabled therefore should return true', function(done) {
+
+  });
+  
+  it('Radio button pumpkin is enabled therefore should be true', function(done) {
+
+  });
+  
+  it('Radio button cabbage is disabled therefore should be false', function(done) {
+
+  });
+});
