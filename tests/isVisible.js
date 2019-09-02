491ccde645e356433d7f7caa8138535eefa1d2e6
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-09-02 16:37:12 -0600

    creat base cases for isVisible tests

---
+++ tests/isVisible.js
@@ -0,0 +1,21 @@
+beforeEach(function() {
+  browser.url("/Hidden-Elements/index.html");
+})
+
+describe('Test whether hidden elements are visible', function() {
+  it('Button display is set to non display therefore should return false', function(done) {
+
+  });
+
+  it('Button display is set to visibility hidden therefore should return false', function(done) {
+
+  });
+
+  it('Button display is set to zero opacity therefore should return false', function(done) {
+
+  });
+
+  it('Header text is visible therefore should return true', function(done) {
+
+  });
+});
