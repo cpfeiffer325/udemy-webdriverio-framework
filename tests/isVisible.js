124d87a51def29820bd11207fd9f024a0a756b48
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-09-02 16:42:50 -0600

    complete isVisible tests for hidden elements page

--- tests/isVisible.js
+++ tests/isVisible.js
@@ -4,18 +4,26 @@ beforeEach(function() {
 
 describe('Test whether hidden elements are visible', function() {
   it('Button display is set to non display therefore should return false', function(done) {
-
+  	var isVisible = browser.isVisible('#not-displayed');
+  	console.log(isVisible);
+  	expect(isVisible).to.equal(false);
   });
 
   it('Button display is set to visibility hidden therefore should return false', function(done) {
-
+  	var isVisible = browser.isVisible('#visibility-hidden');
+  	console.log(isVisible);
+  	expect(isVisible).to.equal(false);
   });
 
   it('Button display is set to zero opacity therefore should return false', function(done) {
-
+  	var isVisible = browser.isVisible('#zero-opacity');
+  	console.log(isVisible);
+  	expect(isVisible).to.equal(false);
   });
 
   it('Header text is visible therefore should return true', function(done) {
-
+  	var isVisible = browser.isVisible('h1');
+  	console.log(isVisible);
+  	expect(isVisible).to.equal(true);
   });
 });
