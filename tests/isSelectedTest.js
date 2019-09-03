119d65d33507358887ce294ad96f1210d56915fc
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-09-02 23:27:33 -0600

    create base file for isSelected tests

---
+++ tests/isSelectedTest.js
@@ -0,0 +1,25 @@
+eforeEach(function() {
+  browser.url("/Dropdown-Checkboxes-RadioButtons/index.html");
+})
+
+describe('Test Selected Dropdown Menus, Checkboxes & Radio Buttons', function() {
+  it('Dropdown item Java is selected therefore should return true', function(done) {
+
+  });
+  
+  it('Dropdown item maven is not selected therefore should return false', function(done) {
+
+  });
+  
+  it('Option2 is not selected therefore should return false', function(done) {
+
+  });
+  
+  it('Option3 is selected therefore should return true', function(done) {
+
+  });
+  
+  it('Radio button pumpkin is selected therefore should be true', function(done) {
+
+  });
+});
