107053a8fb85ccf7f9c10392d4cce1051ae33b19
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-09-05 23:09:57 -0600

    complete combo wait for exist and visible tests

--- tests/waitForExistAndVisibleTest.js
+++ tests/waitForExistAndVisibleTest.js
@@ -4,12 +4,15 @@ beforeEach(function() {
 
 describe('Validate whether the click me button is visible', function() {
   it('Validate whether the click me button code is visible within the DOM', function(done) {
-      this.timeout(20000);
-      var clickMeButton = "//p[text()='CLICK ME!']";
-      browser.waitForExist(clickMeButton, 8000, false);
+    this.timeout(20000);
+    let clickMeButton = "//p[text()='CLICK ME!']";
+    browser.waitForExist(clickMeButton, 8000, false);
   });
 
   it('Validate whether the click me button appears once the Ajax loader fully loads', function(done) {
+    this.timeout(20000);
+    let clickMeButton = "//p[text()='CLICK ME!']";
+    browser.waitForVisible(clickMeButton, 8000, false);
   });
 
 });
