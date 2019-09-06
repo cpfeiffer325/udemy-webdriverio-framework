f60c434adcc8ff60b9aa2c86f3b22a4c18067625
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-09-05 23:11:13 -0600

    create wait until base case

---
+++ tests/waitUntilTest.js
@@ -0,0 +1,9 @@
+beforeEach(function() {
+	browser.url("/Accordion/index.html");
+})
+
+describe('Validate the loading functionality works correctly', function() {
+	it('Verify relevant text LOADING COMPLETE appears after a period of time', function () {
+
+	});
+});
