efb3b9943b4f997ac79273366890d6ab85a60dc9
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-09-05 23:03:36 -0600

    complete waitForText test case

--- tests/waitForTextTest.js
+++ tests/waitForTextTest.js
@@ -3,6 +3,14 @@ beforeEach(function(){
 })
 describe('Verify that the correct text appears on the accordion page', function() {
 	it('Verify loading complete text is visible after a set duration of time', function() {
+    this.timeout(20000);
+    elements = $('#hidden-text');
+    console.log(`Current text ${elements.getText()}`);
+    elements.waitForText(1000);
 
+    while(elements.getText() != "LOADING COMPLETE.") {
+      browser.pause(1000);
+    }
+    console.log(elements.getText())
 	});
 });
