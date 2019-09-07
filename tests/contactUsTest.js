093a0de13d536e85c9f85e0a19599ecbe9e82405
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-09-07 14:40:19 -0600

    define custom commands for contact details on contact us page tests

--- tests/contactUsTest.js
+++ tests/contactUsTest.js
@@ -1,5 +1,21 @@
 let request = require('sync-request');
 
+browser.addCommand("submitDataVioContactUsForm", function (firstName, lastName, email, comments) {
+  if(firstName) {
+    browser.setValue("[name='first_name']", firstName);
+  }
+  if(lastName) {
+    browser.setValue("[name='last_name']", lastName);
+  }
+  if(email) {
+    browser.setValue("[name='email']", email);
+  }
+  if(comments) {
+    browser.setValue("[name='message']", comments);
+  }
+  browser.click("[type='submit']");
+})
+
 beforeEach(function() {
 	browser.url('/Contact-Us/contactus.html');
 })
