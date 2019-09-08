18a34d0149d1e461421d605dc483fe4d13a0164d
Christian Pfeiffer <cpfeiffer325@gmail.com>
2019-09-08 13:29:10 -0600

    update code for isVideoPaused test

--- tests/videoExecuteCommandTest.js
+++ tests/videoExecuteCommandTest.js
@@ -4,28 +4,28 @@ beforeEach(function() {
 
 //Injects a snippet of JavaScript into the page for execution in the context of the currently selected frame.
 browser.addCommand('isVideoPaused', function() {
-    let isPaused = browser.exectute(function() {
-        console.log('Outputted inside the console window');
+	let isPaused = browser.execute(function() {
+		console.log('Outputted inside the console window');
 
-        let video = document.querySelector('#video1');
-        return video.paused;
-    })
-    return isPaused.value;
+		let video = document.querySelector('#video1');
+		return video.paused;
+	})
+	return isPaused.value;
 });
 
 describe('Video test', function() {
 	it('Validate that the video is paused when accessing the page', function(done) {
-        this.timeout(20000);
-        let isPaused = browser.isVideoPaused();
-        expect(isPaused).to.be.true;
-        browser.pause(3000);
+		this.timeout(20000);
+		let isPaused = browser.isVideoPaused();
+		expect(isPaused).to.be.true;
+		browser.pause(6000);
 	});
 
 	it('Alter the width of the video', function(done) {
-        let videoWidth = browser.execute(function() {
-            let video = document.querySelector('#video1');
-            return video.style.width = "300px";
-        });
-        browser.pause(3000);
+		let videoWidth = browser.execute(function() {
+			let video = document.querySelector('#video1');
+			return video.style.width = "300px";
+		});
+		browser.pause(6000);		
 	});
-}); 
+});
