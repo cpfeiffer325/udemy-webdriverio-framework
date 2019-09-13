d4a1c28b3e4576f0d71d6aebb7adf7fac1e256e6
Christian Pfeiffer <cpfeiffer325@gmail.com>
2019-09-13 05:17:04 -0600

    clean up modules and add del dependency

--- wdio.conf.js
+++ wdio.conf.js
@@ -133,7 +133,7 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: http://webdriver.io/guide/reporters/dot.html
-    reporters: ['dot', 'junit', /*'json',*/ 'allure'],
+    reporters: ['dot', 'junit', /*'json', 'allure'*/],
 
     reporterOptions: {
         junit: {
@@ -142,12 +142,12 @@ exports.config = {
         // json: {
         //     outputDir: './reports/json-results/'
         // },
-        allure: {
-            outputDir: './reports/allure-results/',
-            disableWebdriverStepsReporting: false,
-		    disableWebdriverScreenshotsReporting: false,
-		    useCucumberStepReporter: false
-        }
+        // allure: {
+        //     outputDir: './reports/allure-results/',
+        //     disableWebdriverStepsReporting: false,
+		//     disableWebdriverScreenshotsReporting: false,
+		//     useCucumberStepReporter: false
+        // }
     },
     //
     // Options to be passed to Mocha.
@@ -178,8 +178,10 @@ exports.config = {
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {Array.<String>} specs List of spec file paths that are to be run
      */
-    // beforeSession: function (config, capabilities, specs) {
-    // },
+    beforeSession: function (config, capabilities, specs) {
+        const del = require('del');
+        del(['reports', 'errorShots']);
+    },
     /**
      * Gets executed before test execution begins. At this point you can access to all global
      * variables like `browser`. It is the perfect place to define custom commands.
