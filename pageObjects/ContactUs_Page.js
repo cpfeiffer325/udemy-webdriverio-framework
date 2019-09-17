07d177248fc2da837f25d42c73c31e349a982909
Christian Pfeiffer <cpfeiffer325@gmail.com>
2019-09-16 23:11:10 -0600

    refactor code for webdriver version 5

--- pageObjects/ContactUs_Page.js
+++ pageObjects/ContactUs_Page.js
@@ -5,6 +5,10 @@ class ContactUs_Page {
 	get comments() { return $("textarea");}
 	get emailAddress() { return $("[name='email']");}
 	get submitButton() { return $("[type='submit']");}
+	get successfulSubmissionHeader() { return $("#contact_reply h1");}
+	get unsuccessfulSubmissionHeader() { return $("body");}
+	get successfulSubmissionHeaderText() { return this.successfulSubmissionHeader.getText();} // Thank You for your Message!
+	get unsuccessfulSubmissionHeaderText() { return this.unsuccessfulSubmissionHeader.getText();} // Error: all fields are required Error: Invalid email address
 
 	//store functions for all our tests
 	setFirstName(firstName) {
@@ -42,22 +46,6 @@ class ContactUs_Page {
 		}
 		this.submitButton.click();
 		this.confirmSuccessfulSubmission();
-	}
-
-	confirmSuccessfulSubmission() {
-		let successfulSubmissionHeader = "#contact_reply h1";
-		let validateSubmissionHeader = browser.waitUntil(function() {
-			return browser.getText(successfulSubmissionHeader) == 'Thank You for your Message!'
-		}, 3000)
-		expect(validateSubmissionHeader, 'Successful Submission Message does not Exist!').to.be.true;
-	}
-
-	confirmUnsuccessfulSubmission() {
-		let unsuccessfulSubmissionHeader = "body";
-		let validateSubmissionHeader = browser.waitUntil(function() {
-			return browser.getText(unsuccessfulSubmissionHeader) == 'Error: all fields are required'
-		}, 3000)
-		expect(browser.getText(unsuccessfulSubmissionHeader)).to.include('Error: all fields are required');
 	}
 }
 
