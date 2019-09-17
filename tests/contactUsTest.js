7334fdd9fd190af856c4332ec1334f37f11391d3
Christian Pfeiffer <cpfeiffer325@gmail.com>
2019-09-16 23:01:16 -0600

    update contactUs page test

--- tests/contactUsTest.js
+++ tests/contactUsTest.js
@@ -1,33 +1,17 @@
 var ContactUs_Page = require("../pageObjects/ContactUs_Page.js");
 
-beforeEach(function() {
-	browser.url('/Contact-Us/contactus.html');
-})
+beforeEach(async() => {
+	await browser.url('/Contact-Us/contactus.html');
+});
 
 describe('Test Contact Us form WebdriverUni', function() {
-  it('Should be able to submit a successful submission via contact us form', function(done) {
+  it('Test1: Should be able to submit a successful submission via contact us form', function(done) {
 	ContactUs_Page.submitAllInfoViaContactUSForm('joe', 'Blogs', 'joe_blogs123@outlook.com', 'this is a great day to learn coding')
 	});
 
-  it('Should not be able to submit a successful submission via contact us form as all fields are required', function(done) {
-  	ContactUs_Page.setFirstName('Mike');
-  	ContactUs_Page.setLastName('Woods');
-  	ContactUs_Page.setEmailAddress('mike_woods@mail.com');
-  	ContactUs_Page.clickSubmitButton();
-  	ContactUs_Page.confirmUnsuccessfulSubmission();
-    });
-
-  
-  it('Should not be able to submit a successful submission via contact us form as all fields are required', function(done) {
-  	ContactUs_Page.setFirstName('Sarah');
-  	ContactUs_Page.setEmailAddress('sarah_woods@mail.com');
-  	ContactUs_Page.clickSubmitButton();
-  	ContactUs_Page.confirmUnsuccessfulSubmission();
-    });
-
-  it('Should not be able to submit a successful submission via contact us form as all fields are required', function(done) {
-  	ContactUs_Page.setLastName('Jomes');
-  	ContactUs_Page.setEmailAddress('sarah_Jomes@mail.com');
+  it('Test4: Should not be able to submit a successful submission via contact us form as all fields are required', function(done) {
+  	ContactUs_Page.setLastName('Tom');
+  	ContactUs_Page.setEmailAddress('tom_Jomes@mail.com');
   	ContactUs_Page.clickSubmitButton();
   	ContactUs_Page.confirmUnsuccessfulSubmission();
     });
