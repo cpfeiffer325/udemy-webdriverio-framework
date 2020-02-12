7603b8646a4b9bd8820ec4075591387a038aa906
cpfeiffer325 <christian.pfeiffer@payload.com>
2020-02-12 08:34:08 -0700

    install babel and create config file

--- webdriverio-v5/hello-world.js
+++ webdriverio-v5/hello-world.js
@@ -1,4 +1,6 @@
-console.log('Hello World!')
+console.log("Hello World!");
 // npm registry down, need to install npm i --save-dev @wdio/cli
-
-
+// need to run ./node_modules/.bin/wdio config
+// uncomment before function in wdio config and require('bable/register')
+// mocha Ops add line compilers: ['js:@babel/register']
+// http://localhost:4444/wd/hub/static/resource/hub.html
